04f871ded9f1a061d799c2c89e847a587829e0ec
Nathan Heidt <heidt@madeinspace.us>
2020-01-02 22:03:06 -0800

    Now using temperature for pressure calcs, added some orbital calculation info

--- aerobraking.py
+++ aerobraking.py
@@ -1,9 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from collections import namedtuple
 
-# Specific gas constant
-R = 287.053
+# Specific gas constant, this looks like what KSP uses for some reason?  Not the correct constant
+R = 193.359
 
 ATMOSPHERE_TABLE = np.array([
     [0	    ,6755],
@@ -18,6 +19,18 @@ ATMOSPHERE_TABLE = np.array([
     [40000	,4.918],
     [50000	,0     ]
 ])
+
+TEMPERATURE_TABLE = np.array([
+    [0, 240],
+    [5, 235],
+    [10, 225],
+    [20, 170],
+    [25, 150],
+    [45, 150],
+    [50, 175]
+])
+
+OrbitState = namedtuple('OrbitState', ['position', 'velocity', 'r_prev'])
 
 
 class AerobrakeCalculator:
@@ -45,13 +58,14 @@ class AerobrakeCalculator:
         self.T_0 = T_0
         # TODO: calculate the temperature for various altitudes/positions/etc.
         self.get_density = lambda p: p / (R * T_0)
-        self.get_drag = lambda v, p: Cd * self.area * 0.5 * p * v**2.0
 
     def calculate_aero(self, position, velocity, plot=True):
         position = np.array(position)
         velocity = np.array(velocity)
         position_list = []
         time_list = []
+        velocity_list = []
+        density_list = []
 
         r = np.linalg.norm(position)
         r_prev = np.linalg.norm(position)
@@ -73,41 +87,41 @@ class AerobrakeCalculator:
                     apoapsis = True
                 else:
                     periapsis = True
-
-            if not apoapsis and r < r_prev:
+            elif not apoapsis and r < r_prev:
                 apoapsis = True
                 periapsis = False
                 print("Apoapsis at: ", r - self.R)
-
-            if not periapsis and r > r_prev:
+            elif not periapsis and r > r_prev:
                 periapsis = True
                 apoapsis = False
                 print("Periapsis at: ", r - self.R)
                 num_orbits += 1
 
             if r < self.R:
-                print("Crashed at: ", position)
+                print("Crashed at: ", position, " Moving: ", np.linalg.norm(velocity))
                 run = False
 
-            if num_orbits > 10:
+            if num_orbits > 4:
                 run = False
-
-            if iteration % 100000 == 0:
-                print(iteration)
 
             t = iteration * dt
             g = self.mu / (r**2.0)
+            altitude = r - self.R
 
             v = np.linalg.norm(velocity)
+            velocity_list.append(v)
             inverse_normal_position = -position / np.linalg.norm(position)
             inverse_normal_velocity = -velocity / np.linalg.norm(velocity)
             gravity_vec = inverse_normal_position * g
 
             # Pressure from the surface not the center
-            pressure = self.get_pressure(r - self.R)
-            density = self.get_density(pressure)
-            Fdrag = self.get_drag(v, density)
-            if r - self.R < self.H:
+            pressure = self.get_pressure(altitude)
+            density = pressure / (R * self.get_atmospheric_temperature(altitude))
+            density_list.append(density)
+
+            Fdrag = self.Cd * self.area * 0.5 * density * v**2.0
+
+            if altitude < self.H:
                 Fdragvec = inverse_normal_velocity * Fdrag
             else:
                 Fdragvec = np.array([0, 0, 0])
@@ -128,8 +142,29 @@ class AerobrakeCalculator:
 
         position_array = np.array(position_list)
         ax.plot(position_array[:, 0], position_array[:, 1], position_array[:, 2])
+
+        fig2 = plt.figure()
+        plt.plot(density_list)
+
+        fig3 = plt.figure()
+        plt.plot(velocity_list)
+
         plt.show()
+
+    def step_orbit(self, current_state):
+        
 
     def get_pressure(self, altitude):
         return np.interp(altitude, ATMOSPHERE_TABLE[:, 0], ATMOSPHERE_TABLE[:, 1])
+
+    def get_atmospheric_temperature(self, altitude):
+        return np.interp(altitude, TEMPERATURE_TABLE[:, 0], TEMPERATURE_TABLE[:, 1])
+
+    def optimal_aero(self, position, velocity, apoapsis, periapsis, target_latitude, deltav):
+        gradient_change = 1.0
+        # test a new periapsis
+        # get the deltav at the longitude
+            # this deltav is the deltav for horizontal cancel + deltav for vertical burn
+        # use the error between that deltav and the desired to adjust the periapsis
+            # we want the highest periapsis that will yield below desired deltav
 
--- autopilot.py
+++ autopilot.py
@@ -1,6 +1,6 @@
 import krpc
 import aerobraking
-
+import orbital_calculations
 
 MIN_HORIZONTAL_SPEED = 10.0
 MAX_THROTTLE = 1.0
@@ -8,7 +8,8 @@ THROTTLE_OFF = 0.0
 LANDING_SPEED = 6.0
 BREAKING_THREHOLD = 20.0
 VESSEL_SURFACE_AREA = 5.381
-DRAG_COEFF = 170.0
+DRAG_COEFF = 1.5
+BREAKING_THRESHOLD = 20.0
 
 
 class AutoPilot:
@@ -17,6 +18,7 @@ class AutoPilot:
         self.vessel = self.conn.space_center.active_vessel
         self.flight = self.vessel.flight()
         self.autopilot = self.vessel.auto_pilot
+        self.orbit = self.vessel.orbit
 
     def reinit_connection(self):
         self.conn = krpc.connect()
@@ -57,7 +59,7 @@ class AutoPilot:
             breaking = 0.5 * v**2.0 / a
 
             # When we are at the intersection of velocity and distance to do a suicide burn, execute the burn
-            if((breaking - d) < BREAKING_THRESHOLD):
+            if((d - breaking) < BREAKING_THRESHOLD):
                 break
 
         self.vessel.control.throttle = MAX_THROTTLE
@@ -104,7 +106,14 @@ class AutoPilot:
         print(position, velocity)
         aerobrake_calculator.calculate_aero(position, velocity) 
 
+    def calc_periapsis_change(self, change):
+        apoapsis = self.orbit.apoapsis
+        periapsis = self.orbit.periapsis
+        new_periapsis = self.orbit.periapsis + change
+        mu = self.orbit.body.gravitational_parameter
+        deltav = orbital_calculations.apsis_change_dv(mu, apoapsis, periapsis, new_periapsis)
+        return deltav
 
 if __name__ == "__main__":
     sc = AutoPilot()
-    sc.plot_orbit()
+    
